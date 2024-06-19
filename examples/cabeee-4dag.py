77b83636c4c62e3d0badd25b17906d5a08ade8f6
Carlos Barrios <cabeee@rit.edu>
2024-06-19 14:14:32 -0400

    added custom consumer app along with 4dag scenario and workflows. Attaching application parameters from workflow file works. wip: custom producer and forwarder apps.

--- examples/cabeee-4dag.py
+++ examples/cabeee-4dag.py
@@ -21,7 +21,8 @@
 # along with Mini-NDN, e.g., in COPYING.md file.
 # If not, see <http://www.gnu.org/licenses/>.
 
-from subprocess import PIPE
+#from subprocess import PIPE
+import subprocess
 
 from mininet.log import setLogLevel, info
 from mininet.topo import Topo
@@ -36,13 +37,16 @@ import argparse
 from minindn.apps.nlsr import Nlsr
 from minindn.helpers.ip_routing_helper import IPRoutingHelper
 from minindn.helpers.merge_nfd_logs import MergeNFDLogs
+from minindn.util import copyExistentFile
 
-PREFIX = "/example"
+from time import sleep
 
-def printOutput(output):
-    _out = output.decode("utf-8").split("\n")
-    for _line in _out:
-        info(_line + "\n")
+import sys
+
+
+PREFIX = "/interCACHE"
+WORKFLOW = "/home/cabeee/mini-ndn/workflows/4dag.json"
+
 
 def run():
     Minindn.cleanUp()
@@ -50,51 +54,24 @@ def run():
 
     MergeNFDLogs.deleteAllLogs()
 
-    # the following parser came from static_routing_experiment.py
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--face-type', dest='faceType', default='udp', choices=['udp', 'tcp'])
-    parser.add_argument('--routing', dest='routingType', default='link-state',
-                         choices=['link-state', 'hr', 'dry'],
-                         help='''Choose routing type, dry = link-state is used
-                                 but hr is calculated for comparision.''')
 
 
-
-    '''
-    # Topology can be created/modified using Mininet topo object
-    topo = Topo()
-    info("Setup\n")
-    # add hosts
-    a = topo.addHost('a')
-    b = topo.addHost('b')
-    c = topo.addHost('c')
-
-    # add links
-    topo.addLink(a, b, delay='10ms', bw=10) # bw = bandwidth
-    topo.addLink(b, c, delay='10ms', bw=10)
-    '''
-
-    ndn = Minindn(parser=parser, topoFile='topologies/cabeee-3node.conf')
-    #ndn = Minindn(parser=parser, topoFile='topologies/cabeee-3node-slow.conf')
-    #ndn = Minindn(topoFile='topologies/cabeee-default-topology.conf')
-    ndn.start()
-
-    # configure and start nfd on each node
-    info("Configuring NFD\n")
-    AppManager(ndn, ndn.net.hosts, Nfd, logLevel="DEBUG")
-
-
-
-    routeOption = 2
 
     """
     There are multiple ways of setting up routes in Mini-NDN
     refer: https://minindn.memphis.edu/experiment.html#routing-options
     """
+    routeOption = 2
 
-    if routeOption == 1:
-        # OPTION 1
+
+    if routeOption == 0:
+        # OPTION 0
         # set up routes manually. The important bit to note here is the use of the Nfdc command
+
+        ndn = Minindn(topoFile='topologies/cabeee-3node.conf')
+        #ndn = Minindn(topoFile='topologies/cabeee-3node-slow.conf')
+        ndn.start()
+
         info('Setting up routes manually in NFD\n')
         #links = {"sensor":["rtr1"], "rtr1":["rtr2"], "rtr2":["rtr3"], "rtr3":["orch"], "orch":["user"]} # routes are directional! This is the wrong direction.
         links = {"user":["orch"], "orch":["rtr3"], "rtr3":["rtr2"], "rtr2":["rtr1"], "rtr1":["sensor"]}
@@ -107,15 +84,51 @@ def run():
                 Nfdc.createFace(host1, interface_ip)
                 Nfdc.registerRoute(host1, PREFIX, interface_ip, cost=0)
 
+        sleep(1)
+
+    if routeOption == 1:
+        # OPTION 1
+        # set up routes using Link State Routing (NLSR).
+
+        ndn = Minindn(topoFile='topologies/cabeee-3node.conf')
+        #ndn = Minindn(topoFile='topologies/cabeee-3node-slow.conf')
+        ndn.start()
+
+        info('Adding IP routes to NFD\n')
+        #info('Starting NFD on nodes\n')
+        nfds = AppManager(ndn, ndn.net.hosts, Nfd, logLevel="DEBUG")
+        info('Starting NLSR on nodes\n')
+        nlsrs = AppManager(ndn, ndn.net.hosts, Nlsr)
+
+        sleep(90)
+
     if routeOption == 2:
         # OPTION 2
-        # or use static routing as used in static_routing_experiment.py
+        # use static routing as used in static_routing_experiment.py
+    
+        # the following parser came from static_routing_experiment.py
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--face-type', dest='faceType', default='udp', choices=['udp', 'tcp'])
+        parser.add_argument('--routing', dest='routingType', default='link-state',
+                            choices=['link-state', 'hr', 'dry'],
+                            help='''Choose routing type, dry = link-state is used
+                                    but hr is calculated for comparision.''')
+
+        ndn = Minindn(parser=parser, topoFile='topologies/cabeee-3node.conf')
+        #ndn = Minindn(parser=parser, topoFile='topologies/cabeee-3node-slow.conf')
+        #ndn = Minindn(topoFile='topologies/cabeee-default-topology.conf')
+        ndn.start()
+
+        # configure and start nfd on each node
+        info("Configuring NFD\n")
+        nfds = AppManager(ndn, ndn.net.hosts, Nfd, logLevel="DEBUG")
+
         info('Adding static routes to NFD\n')
         grh = NdnRoutingHelper(ndn.net, ndn.args.faceType, ndn.args.routingType)
         # For all host, pass ndn.net.hosts or a list, [ndn.net['a'], ..] or [ndn.net.hosts[0],.]
         grh.addOrigin([ndn.net['sensor']], [PREFIX])
         grh.calculateNPossibleRoutes()
-        #PREFIX is advertised from node A, it should be reachable from all other nodes.
+        #PREFIX is advertised from node "sensor", it should be reachable from all other nodes.
         routesFromSensor = ndn.net['sensor'].cmd("nfdc route | grep -v '/localhost/nfd'")
         if '/ndn/rtr1-site/rtr1' not in routesFromSensor or \
            '/ndn/rtr2-site/rtr2' not in routesFromSensor or \
@@ -123,79 +136,106 @@ def run():
            '/ndn/orch-site/orch' not in routesFromSensor or \
            '/ndn/user-site/user' not in routesFromSensor:
             info("Route addition failed\n")
-        routesToPrefix = ndn.net['rtr1'].cmd("nfdc fib | grep '/example'")
-        if '/example' not in routesToPrefix:
+        routesToPrefix = ndn.net['rtr1'].cmd("nfdc fib | grep '/interCACHE'")
+        if '/interCACHE' not in routesToPrefix:
             info("Missing route to advertised prefix, Route addition failed\n")
             ndn.net.stop()
             sys.exit(1)
-        routesToPrefix = ndn.net['rtr2'].cmd("nfdc fib | grep '/example'")
-        if '/example' not in routesToPrefix:
+        routesToPrefix = ndn.net['rtr2'].cmd("nfdc fib | grep '/interCACHE'")
+        if '/interCACHE' not in routesToPrefix:
             info("Missing route to advertised prefix, Route addition failed\n")
             ndn.net.stop()
             sys.exit(1)
-        routesToPrefix = ndn.net['rtr3'].cmd("nfdc fib | grep '/example'")
-        if '/example' not in routesToPrefix:
+        routesToPrefix = ndn.net['rtr3'].cmd("nfdc fib | grep '/interCACHE'")
+        if '/interCACHE' not in routesToPrefix:
             info("Missing route to advertised prefix, Route addition failed\n")
             ndn.net.stop()
             sys.exit(1)
-        routesToPrefix = ndn.net['orch'].cmd("nfdc fib | grep '/example'")
-        if '/example' not in routesToPrefix:
+        routesToPrefix = ndn.net['orch'].cmd("nfdc fib | grep '/interCACHE'")
+        if '/interCACHE' not in routesToPrefix:
             info("Missing route to advertised prefix, Route addition failed\n")
             ndn.net.stop()
             sys.exit(1)
         info('Route addition to NFD completed\n')
 
-
+        sleep(1)
 
     if routeOption == 3:
         # OPTION 3
-        # or use IP routing as used in ip_rounting_experiment.py
-        # NOTE: this will only work if we send pings based on IP address (as done in
-        # dl/mininet/mininet/net.py ping function)
+        # use IP routing as used in ip_rounting_experiment.py
+
+        ndn = Minindn(topoFile='topologies/cabeee-3node.conf')
+        #ndn = Minindn(topoFile='topologies/cabeee-3node-slow.conf')
+        #ndn = Minindn(topoFile='topologies/cabeee-default-topology.conf')
+        ndn.start()
+
+        # NOTE: this method is also used in traffic_generator.py, pcap_logging_experiment.py, and consumer-producer.py
         info('Adding IP routes to NFD\n')
         #info('Starting NFD on nodes\n')
-        #nfds = AppManager(ndn, ndn.net.hosts, Nfd)
+        nfds = AppManager(ndn, ndn.net.hosts, Nfd, logLevel="DEBUG")
         info('Starting NLSR on nodes\n')
         nlsrs = AppManager(ndn, ndn.net.hosts, Nlsr)
 
         # Calculate all routes for IP routing
         IPRoutingHelper.calcAllRoutes(ndn.net)
-        info("IP routes configured, start ping\n")
+        info("IP routes configured\n")
+
+        sleep(90)
 
 
 
 
 
-    # Start ping server
-    info("Starting pings...\n")
-    pingserver_log = open("{}/sensor/ndnpingserver.log".format(ndn.workDir), "w")
-    getPopen(ndn.net["sensor"], "ndnpingserver {}".format(PREFIX), stdout=pingserver_log,\
-             stderr=pingserver_log)
 
-    # start ping client
-    ping1 = getPopen(ndn.net["user"], "ndnping {} -c 5".format(PREFIX), stdout=PIPE, stderr=PIPE)
-    ping1.wait()
-    printOutput(ping1.stdout.read())
 
-    '''
-    interface = ndn.net["rtr3"].connectionsTo(ndn.net["rtr2"])[0][0]
-    info("Failing link\n") # failing link by setting link loss to 100%
-    interface.config(delay="10ms", bw=10, loss=100)
-    info ("\n starting ping2 client \n")
+    # SET UP THE PRODUCER
+    info('Starting Producer App\n')
 
-    ping2 = getPopen(ndn.net["user"], "ndnping {} -c 5".format(PREFIX), stdout=PIPE, stderr=PIPE)
-    ping2.wait()
-    printOutput(ping2.stdout.read())
+    # choice 1: (runs in the background so that it is non-blocking)
+    # App input is the service PREFIX
+    cmd = '/home/cabeee/mini-ndn/dl/ndn-cxx/build/examples/cabeee-custom-app-producer {} > cabeee_producer.log &'.format(PREFIX + "/service4")
+    #cmd = '/home/cabeee/mini-ndn/dl/ndn-cxx/build/examples/cabeee-custom-app-producer > cabeee_producer.log &'
+    producer  = ndn.net['sensor']
+    producer.cmd(cmd)
+    
+    # or one-liner
+    #ndn.net['sensor'].cmd("/home/cabeee/mini-ndn/examples/apps/dist/cabeee-custom-app-producer")
 
-    interface.config(delay="10ms", bw=10, loss=0) # bringing back the link
-    '''
 
-    # concatenate every node's log/nfd.log file to a single one. Keep timestamp, add node name. Sort by timestamp!
-    MergeNFDLogs.mergeAllLogs()
+    sleep(1)
+
+
+    # SET UP THE FORWARDERS
+    # TODO: run the cabeee-dag-forwarder-app application on all router nodes
+
+
+    # SET UP THE CONSUMER
+    info('Starting Consumer App\n')
+
+    # App input is the main PREFIX, the workflow file, and the orchestration value (0, 1 or 2)
+    cmd = '/home/cabeee/mini-ndn/dl/ndn-cxx/build/examples/cabeee-custom-app-consumer {} {} {} > cabeee_consumer.log &'.format(PREFIX, WORKFLOW, 0)
+    #cmd = '/home/cabeee/mini-ndn/dl/ndn-cxx/build/examples/cabeee-custom-app-consumer > cabeee_consumer.log &'
+    #cmd = '/home/cabeee/mini-ndn/dl/ndn-cxx/build/examples/cabeee-custom-app-consumer > cabeee_consumer.log'
+    #cmd = '/home/cabeee/mini-ndn/dl/ndn-cxx/build/examples/cabeee-custom-app-consumer &'
+    #cmd = '/home/cabeee/mini-ndn/dl/ndn-cxx/build/examples/cabeee-custom-app-consumer'
+    consumer  = ndn.net['user']
+    consumer.cmd(cmd)
+
+
+    sleep(1)
+
+
+
+
+
+
 
     info("\nExperiment Completed!\n")
     MiniNDNCLI(ndn.net)
     ndn.stop()
+
+    # concatenate every node's log/nfd.log file to a single one. Keep timestamp, add node name. Sort by timestamp!
+    MergeNFDLogs.mergeAllLogs()
 
 if __name__ == '__main__':
     setLogLevel("info")
