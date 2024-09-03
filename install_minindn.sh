96f0c90da2bf4e96162a626eef787dbb73229b34
Ethan Alker <eda5693@rit.edu>
2024-09-03 15:31:45 -0400

    update install script

--- install_minindn.sh
+++ install_minindn.sh
@@ -1,45 +1,41 @@
-#! /bin/bash
+#!/bin/bash
 
+set -eou pipefail
 
-INSTALL_DIR=~
+INSTALL_DIR="$HOME"
 #INSTALL_DIR=/opt/ndn/
 
-cd $INSTALL_DIR
+mkdir -p "$INSTALL_DIR"
 
-git clone git@github.com:rit-ndn/mini-ndn.git
-cd mini-ndn
-./install.sh --source
+# make sure your user has read/write access to the repo
+git clone git@github.com:rit-ndn/mini-ndn.git "$INSTALL_DIR/mini-ndn"
+cd "$INSTALL_DIR/mini-ndn"
+./install.sh --source -y
 
 # after mini-ndn installs, must go in and change the git repos for the sub-modules
-cd dl
-git clone git@github.com:rit-ndn/NFD.git
-git clone git@github.com:rit-ndn/ndn-cxx.git
-git clone git@github.com:rit-ndn/NLSR.git
+git clone git@github.com:rit-ndn/NFD.git "$INSTALL_DIR/mini-ndn/dl/NFD"
+git clone git@github.com:rit-ndn/ndn-cxx.git "$INSTALL_DIR/mini-ndn/dl/ndn-cxx"
+git clone git@github.com:rit-ndn/NLSR.git "$INSTALL_DIR/mini-ndn/dl/NLSR"
 
 # now we can compile the sub-modules
-cd NFD
+cd "$INSTALL_DIR/mini-ndn/dl/NFD"
 git submodule update --init
 ./waf configure
 ./waf
-sudo .waf install
+sudo ./waf install
 
-cd ../ndn-cxx
+cd "$INSTALL_DIR/mini-ndn/dl/ndn-cxx"
 ./waf configure --with-examples
 ./waf
 sudo ./waf install
 
-cd ../NLSR
+cd "$INSTALL_DIR/mini-ndn/dl/NLSR"
 ./waf configure
 ./waf
 sudo ./waf install
 
 sudo ldconfig
 
-# now we can run the emulator
-cd ../../
+# now we can run the emulator, if we want
+cd "$INSTALL_DIR/mini-ndn"
 #./dag_run.sh
-
-
-
-
-
