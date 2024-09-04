93b878c10f9d93f12c71cee26a366062b3e4ec7d
Ethan Alker <eda5693@rit.edu>
2024-09-03 20:14:29 -0400

    generate perf csv in dag_run

--- dag_run.sh
+++ dag_run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-
 #----- THIS ONLY NEEDS TO BE RUN ONCE --------------------
 #cd ~/mini-ndn/dl/ndn-cxx
 #./waf clean
 #./waf
 #sudo ./waf install
 #---------------------------------------------------------
+
+set -eou pipefail
+
+MININDN_HOME="$HOME/mini-ndn"
 
 script_dir="$MININDN_HOME/examples"
 scripts=(
@@ -40,10 +43,31 @@ scripts=(
 
 #clear
 
+example_log="$MININDN_HOME/example.log"
+consumer_log="/tmp/minindn/user/cabeee_consumer.log"
+csv_out="$MININDN_HOME/$(date +'%Y%m%d%H%M%S')-perf.csv"
+header="Example, Interest Packets Generated, Data Packets Generated, Interest Packets Transmitted, Data Packets Transmitted, Final Result, Service Latency"
+
+echo "$header" > "$csv_out"
+
 for script in "${scripts[@]}"
 do
-        sudo -E python "$script_dir/$script"
-	# show the consumer log (so we can see the final answer and the service latency)
-        cat /tmp/minindn/user/cabeee_consumer.log
-	#cat /tmp/minindn/user/cabeee_consumer2.log
+	# shellcheck disable=SC2024
+        sudo -E python "$script_dir/$script" > "$example_log"
+	packets=$(sed -n \
+		-e 's/^Interest Packets Generated: \([0-9]*\) interests$/\1, /p' \
+		-e 's/^Data Packets Generated: \([0-9]*\) data$/\1, /p' \
+		-e 's/^Interest Packets Transmitted: \([0-9]*\) interests$/\1, /p' \
+		-e 's/^Data Packets Transmitted: \([0-9]*\) data/\1/p' \
+		"$example_log" \
+		| tr -d '\n' \
+	)
+	consumer_cols=$(sed -n \
+		-e 's/^\s*The final answer is: \([0-9]*\)$/\1, /p' \
+		-e 's/^\s*Service Latency: \([0-9\.]*\) seconds.$/\1/p' \
+		"$consumer_log" \
+		| tr -d '\n' \
+	)
+	
+	echo "$script, $packets, $consumer_cols" >> $csv_out
 done
