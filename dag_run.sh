f77befd3f4c1476fd77c4f5661a9cf58260fdbdd
Ethan Alker <eda5693@rit.edu>
2024-09-04 18:24:23 -0400

    update perf csv
    Update the perf csv with timestamps and git commit hashes of each repo. Also,
    when an example is already present in the csv that row will be replaced
    with data from the most recent run.

--- dag_run.sh
+++ dag_run.sh
@@ -7,7 +7,7 @@
 #sudo ./waf install
 #---------------------------------------------------------
 
-set -eou pipefail
+set -e
 
 MININDN_HOME="$HOME/mini-ndn"
 
@@ -45,29 +45,63 @@ scripts=(
 
 example_log="$MININDN_HOME/example.log"
 consumer_log="/tmp/minindn/user/cabeee_consumer.log"
-csv_out="$MININDN_HOME/$(date +'%Y%m%d%H%M%S')-perf.csv"
-header="Example, Interest Packets Generated, Data Packets Generated, Interest Packets Transmitted, Data Packets Transmitted, Final Result, Service Latency"
+csv_out="$MININDN_HOME/perf-results.csv"
+header="Example, Time, mini-ndn commit, ndn-cxx commit, NFD commit, NLSR commit, Interest Packets Generated, Data Packets Generated, Interest Packets Transmitted, Data Packets Transmitted, Service Latency, Final Result"
 
-echo "$header" > "$csv_out"
+if [ ! -f "$csv_out" ]; then
+	echo "$header" > "$csv_out"
+elif ! grep -q -F "$header" "$csv_out"; then
+	echo "Overwriting csv..."
+	mv "$csv_out" "$csv_out.bak"
+	echo "$header" > "$csv_out"
+fi
 
 for script in "${scripts[@]}"
 do
+	now="$(date -Iseconds)"
+
+	minindn_hash="$(git -C "$MININDN_HOME" rev-parse HEAD)"
+	ndncxx_hash="$(git -C "$MININDN_HOME/dl/ndn-cxx" rev-parse HEAD)"
+	nfd_hash="$(git -C "$MININDN_HOME/dl/NFD" rev-parse HEAD)"
+	nlsr_hash="$(git -C "$MININDN_HOME/dl/NLSR" rev-parse HEAD)"
+
+	# these sed scripts depend on the order in which the logs are printed
+
 	# shellcheck disable=SC2024
         sudo -E python "$script_dir/$script" > "$example_log"
 	packets=$(sed -n \
-		-e 's/^Interest Packets Generated: \([0-9]*\) interests$/\1, /p' \
-		-e 's/^Data Packets Generated: \([0-9]*\) data$/\1, /p' \
-		-e 's/^Interest Packets Transmitted: \([0-9]*\) interests$/\1, /p' \
-		-e 's/^Data Packets Transmitted: \([0-9]*\) data/\1/p' \
+		-e 's/^Interest Packets Generated: \([0-9]*\) interests$/\1,/p' \
+		-e 's/^Data Packets Generated: \([0-9]*\) data$/\1,/p' \
+		-e 's/^Interest Packets Transmitted: \([0-9]*\) interests$/\1,/p' \
+		-e 's/^Data Packets Transmitted: \([0-9]*\) data/\1,/p' \
 		"$example_log" \
 		| tr -d '\n' \
 	)
-	consumer_cols=$(sed -n \
-		-e 's/^\s*The final answer is: \([0-9]*\)$/\1, /p' \
-		-e 's/^\s*Service Latency: \([0-9\.]*\) seconds.$/\1/p' \
+
+	interest_gen="$(echo "$packets" | cut -d',' -f1)"
+	data_gen="$(echo "$packets" | cut -d',' -f2)"
+	interest_trans="$(echo "$packets" | cut -d',' -f3)"
+	data_trans="$(echo "$packets" | cut -d',' -f4)"
+
+	consumer_parse=$(sed -n \
+		-e 's/^\s*The final answer is: \([0-9]*\)$/\1,/p' \
+		-e 's/^\s*Service Latency: \([0-9\.]*\) seconds.$/\1,/p' \
 		"$consumer_log" \
 		| tr -d '\n' \
 	)
-	
-	echo "$script, $packets, $consumer_cols" >> $csv_out
+
+	result="$(echo "$consumer_parse" | cut -d',' -f1)"
+	latency="$(echo "$consumer_parse" | cut -d',' -f2)"
+
+	row="$script, $now, $minindn_hash, $ndncxx_hash, $nfd_hash, $nlsr_hash, $interest_gen, $data_gen, $interest_trans, $data_trans, $latency, $result"
+
+	# WARN: $script is NOT escaped, so don't use any fancy characters
+	# the dot in the file extension technically matches any character, but this shouldn't matter
+	# basically this is fragile, don't be weird with these strings
+	line_num="$(grep -n -F "$script," "$csv_out" | cut -d: -f1 | head -1)"
+	if [ -n "$line_num" ]; then
+		sed --in-place -e "${line_num}c\\$row" "$csv_out"
+	else
+		echo "$row" >> "$csv_out"
+	fi
 done
